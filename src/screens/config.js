c984dd3a9bbe76cd472dfc5fa91d19f3232e974c
Brijesh Saraswat <36710430+brijeshsaraswat@users.noreply.github.com>
2020-07-15 14:43:12 +0530

    Update config.js
    firebase configuration hidden

--- src/screens/config.js
+++ src/screens/config.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 import 'firebase/firestore';
 let config = {
-    apiKey: "AIzaSyBsE_QCYOaplKio1E_X8VhcfXw5axy-o5Y",
-    authDomain: "todos-b29f3.firebaseapp.com",
-    databaseURL: "https://todos-b29f3.firebaseio.com",
-    projectId: "todos-b29f3",
-    storageBucket: "todos-b29f3.appspot.com",
-    messagingSenderId: "752139128528",
-    appId: "1:752139128528:web:e34d5b455f3f21d7e958d5",
-    measurementId: "G-2K68WDPYRM"
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXX.firebaseapp.com",
+    databaseURL: "https://XXXXXXXXXXX.firebaseio.com",
+    projectId: "XXXXXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXX.appspot.com",
+    messagingSenderId: "XXXXXXXXXXXXXXX",
+    appId: "1:xxxxxxxxxxxxx:web:xxxxxxxxxxxxxxxxxxxxxx",
+    measurementId: "XXXXXXXXXXXXXXXXX"
 };
 firebase.initializeApp(config);
 export const db = firebase.firestore();
